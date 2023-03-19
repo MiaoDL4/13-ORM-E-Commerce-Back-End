1c3edb4ae08c9317e3fcc296d972e96cf7fb058b
MLD4 <dongmiao123@gmail.com>
2023-03-19 21:29:01 +1030

    post put delete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -116,6 +116,19 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {id: req.params.id}
+  })
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ message: 'No Product found with this id' });
+        return;
+      }
+      res.json(data);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,14 +37,49 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    Tag: req.body.tag_name
+  })
+    .then(data => res.status(200).json(data))
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: { id: req.params.id },
+  })
+    .then(data => {
+      if (!data[0]) {
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+      }
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: { id: req.params.id },
+  })
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+      }
+      res.json(data);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+
 });
 
 module.exports = router;
